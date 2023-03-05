4775abf2f2a62c9585bb7185ee9b521800e16ff3
sanjanadhenge <dhengesanjana2001@gmail.com>
2023-03-05 19:54:39 +0530

    [sanjana] Add : Calculate length using formula and added branch

--- LengthCalculator/Program.cs
+++ LengthCalculator/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LengthCalculator
+{
+    public class program
+    {
+        static void Main(string[] args)
+        {
+            lengthcalculator lengthcalculator = new lengthcalculator();
+            lengthcalculator.calculatelength();
+        }
+    }
+}
